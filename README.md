2f799c1b5e811031bd7fe7c5260c521fc41eb57a
sigurd4 <sigurd-s@online.no>
2025-04-23 02:58:30 +0200

    Add README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+[![Build Status (nightly)](https://github.com/sigurd4/oscillation/workflows/Build-nightly/badge.svg)](https://github.com/sigurd4/oscillation/actions/workflows/build-nightly.yml)
+[![Build Status (nightly, all features)](https://github.com/sigurd4/oscillation/workflows/Build-nightly-all-features/badge.svg)](https://github.com/sigurd4/oscillation/actions/workflows/build-nightly-all-features.yml)
+
+[![Build Status (stable)](https://github.com/sigurd4/oscillation/workflows/Build-stable/badge.svg)](https://github.com/sigurd4/oscillation/actions/workflows/build-stable.yml)
+[![Build Status (stable, all features)](https://github.com/sigurd4/oscillation/workflows/Build-stable-all-features/badge.svg)](https://github.com/sigurd4/oscillation/actions/workflows/build-stable-all-features.yml)
+
+[![Test Status](https://github.com/sigurd4/oscillation/workflows/Test/badge.svg)](https://github.com/sigurd4/oscillation/actions/workflows/test.yml)
+[![Lint Status](https://github.com/sigurd4/oscillation/workflows/Lint/badge.svg)](https://github.com/sigurd4/oscillation/actions/workflows/lint.yml)
+
+[![Latest Version](https://img.shields.io/crates/v/oscillation.svg)](https://crates.io/crates/oscillation)
+[![License:MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![Documentation](https://img.shields.io/docsrs/oscillation)](https://docs.rs/oscillation)
+[![Coverage Status](https://img.shields.io/codecov/c/github/sigurd4/oscillation)](https://app.codecov.io/github/sigurd4/oscillation)
+
+# oscillation
+
+Oscillators and a collection of waveforms for real-time usage.
--- src/plot.rs
+++ src/plot.rs
@@ -7,7 +7,6 @@ const PLOT_CAPTION_FONT: (&str, u32) = ("sans", 20);
 const PLOT_MARGIN: u32 = 5;
 const PLOT_LABEL_AREA_SIZE: u32 = 30;
 
-#[allow(unexpected_cfgs)]
 pub fn plot_curves_anim<const N: usize, const M: usize>(
     plot_title: &str, plot_path: &str,
     x: [[&[T]; N]; M],
@@ -15,59 +14,62 @@ pub fn plot_curves_anim<const N: usize, const M: usize>(
     time: T
 ) -> Result<(), Box<dyn std::error::Error>>
 {
-    #[cfg(not(tarpaulin))]
+    #[allow(unexpected_cfgs)]
     {
-        let x_min = x.into_iter().flatten().flatten().copied().reduce(T::min).unwrap();
-        let x_max = x.into_iter().flatten().flatten().copied().reduce(T::max).unwrap();
-        
-        let y_min = y.into_iter().flatten().flatten().copied().reduce(T::min).unwrap();
-        let y_max = y.into_iter().flatten().flatten().copied().reduce(T::max).unwrap();
-        
-        let show = (1000.0*time/M as T) as u32;
-
-        let area = BitMapBackend::gif(
-                plot_path,
-                PLOT_RES,
-                show
-            ).unwrap()
-            .into_drawing_area();
-        
-        for (x, y) in x.into_iter().zip(y)
+        #[cfg(not(tarpaulin))]
         {
-            area.fill(&WHITE)?;
+            let x_min = x.into_iter().flatten().flatten().copied().reduce(T::min).unwrap();
+            let x_max = x.into_iter().flatten().flatten().copied().reduce(T::max).unwrap();
             
-            let mut chart = ChartBuilder::on(&area)
-                .caption(plot_title, PLOT_CAPTION_FONT.into_font())
-                .margin(PLOT_MARGIN)
-                .x_label_area_size(PLOT_LABEL_AREA_SIZE)
-                .y_label_area_size(PLOT_LABEL_AREA_SIZE)
-                .build_cartesian_2d(x_min..x_max, y_min..y_max)?;
+            let y_min = y.into_iter().flatten().flatten().copied().reduce(T::min).unwrap();
+            let y_max = y.into_iter().flatten().flatten().copied().reduce(T::max).unwrap();
             
-            chart.configure_mesh()
-                .set_all_tick_mark_size(0.1)
-                .draw()?;
+            let show = (1000.0*time/M as T) as u32;
+
+            let area = BitMapBackend::gif(
+                    plot_path,
+                    PLOT_RES,
+                    show
+                ).unwrap()
+                .into_drawing_area();
             
-            for (i, (x, y)) in x.into_iter().zip(y).enumerate()
+            for (x, y) in x.into_iter().zip(y)
             {
-                let mut j = i;
-                if j == 2
+                area.fill(&WHITE)?;
+                
+                let mut chart = ChartBuilder::on(&area)
+                    .caption(plot_title, PLOT_CAPTION_FONT.into_font())
+                    .margin(PLOT_MARGIN)
+                    .x_label_area_size(PLOT_LABEL_AREA_SIZE)
+                    .y_label_area_size(PLOT_LABEL_AREA_SIZE)
+                    .build_cartesian_2d(x_min..x_max, y_min..y_max)?;
+                
+                chart.configure_mesh()
+                    .set_all_tick_mark_size(0.1)
+                    .draw()?;
+                
+                for (i, (x, y)) in x.into_iter().zip(y).enumerate()
                 {
-                    j += 1
+                    let mut j = i;
+                    if j == 2
+                    {
+                        j += 1
+                    }
+                    let color = Palette99::pick(j);
+                    chart.draw_series(LineSeries::new(
+                            x.into_iter().copied().zip(y.into_iter().copied()),
+                            &color
+                        ))?
+                        .label(format!("{}", i))
+                        .legend(move |(x, y)| Rectangle::new([(x + 5, y - 5), (x + 15, y + 5)], color.mix(0.5).filled()));
                 }
-                let color = Palette99::pick(j);
-                chart.draw_series(LineSeries::new(
-                        x.into_iter().copied().zip(y.into_iter().copied()),
-                        &color
-                    ))?
-                    .label(format!("{}", i))
-                    .legend(move |(x, y)| Rectangle::new([(x + 5, y - 5), (x + 15, y + 5)], color.mix(0.5).filled()));
+                
+                chart.configure_series_labels()
+                    .border_style(&BLACK)
+                    .draw()?;
+                
+                area.present().expect("Unable to write result to file");   
             }
-            
-            chart.configure_series_labels()
-                .border_style(&BLACK)
-                .draw()?;
-            
-            area.present().expect("Unable to write result to file");   
         }
     }
 
